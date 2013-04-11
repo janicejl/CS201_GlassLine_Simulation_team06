d08e9021334fd0e59a70d74143c93c1bed00fdd2
rtani1123 <ryan.tani1123@gmail.com>
2013-04-11 13:28:48 -0700

    Upgrade to GlassSelectPanel GUI. Still non-functional.

--- GlassLine/src/gui/panels/subcontrolpanels/GlassSelectPanel.java
+++ GlassLine/src/gui/panels/subcontrolpanels/GlassSelectPanel.java
@@ -6,6 +6,7 @@ import java.awt.*;
 import gui.panels.ControlPanel;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 
 /**
  * The GlassSelectPanel class contains buttons allowing the user to select what
@@ -19,12 +20,15 @@ public class GlassSelectPanel extends JPanel
 
 	GridBagConstraints gbc = new GridBagConstraints();
 	JComboBox settingSelect = new JComboBox();
+	JPanel settingPanel = new JPanel();
 	JPanel checkBoxPanel = new JPanel();
 	JPanel quantityPanel = new JPanel();
 	JLabel quantityLabel = new JLabel("Number of pieces:");
+	JLabel settingLabel = new JLabel("Setting Name:");
 	JTextField quantityField = new JTextField(10);
+	JTextField settingNameField = new JTextField(10);
 	JButton acceptButton = new JButton("Create");
-	JButton newPartButton = new JButton("New Part");
+	JButton saveButton = new JButton("Save Setting");
 	
 	JPanel[] partPanel = new JPanel[10];
 	JLabel[] partLabels = new JLabel[10];
@@ -61,17 +65,31 @@ public class GlassSelectPanel extends JPanel
 			partPanel[i].setBackground(new Color(238,238,238));
 		}
 		
-		checkBoxPanel.setLayout(new GridLayout(1,2));
+		checkBoxPanel.setLayout(new GridLayout(1,3));
 		JPanel leftPanel = new JPanel();
 		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
-		JPanel rightPanel = new JPanel();
-		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+		JPanel midPanel = new JPanel();
+		midPanel.setLayout(new BoxLayout(midPanel, BoxLayout.Y_AXIS));
 		for(int i = 0; i < 5; i++) {
 			leftPanel.add(partPanel[i]);
-			rightPanel.add(partPanel[i+5]);
+			midPanel.add(partPanel[i+5]);
 		}
+		JPanel rightPanel = new JPanel();
+		GridBagConstraints g1 = new GridBagConstraints();
+		rightPanel.setLayout(new GridBagLayout());
+		g1.gridx = 0;
+		g1.gridy = 0;
+		rightPanel.add(settingLabel, g1);
+		settingNameField.setMaximumSize(new Dimension(100,30));
+		settingNameField.setPreferredSize(new Dimension(100,30));
+		settingNameField.setMinimumSize(new Dimension(100,30));
+		g1.gridy++;
+		rightPanel.add(settingNameField, g1);
+		g1.gridy++;
+		rightPanel.add(saveButton,g1);
 		checkBoxPanel.add(leftPanel);
-		checkBoxPanel.add(rightPanel);	
+		checkBoxPanel.add(midPanel);
+		checkBoxPanel.add(rightPanel);
 		
 		quantityPanel.add(quantityLabel);
 		quantityPanel.add(quantityField);
@@ -85,7 +103,7 @@ public class GlassSelectPanel extends JPanel
 		gbc.gridx = 0;
 		gbc.gridy = 0;
 		gbc.gridwidth = 2;
-		gbc.ipadx = 400;
+		gbc.ipadx = 350;
 		add(settingSelect, gbc);
 
 		gbc.gridy++;
